860e6719f142119c69127289590bbe35738e7e14
Ian Donaldson <imdonaldson@ualr.edu>
2020-10-11 19:49:23 -0700

    initial Commit

---
+++ src/ConnectionFactory.java
@@ -0,0 +1,77 @@
+import com.mysql.jdbc.Driver;
+
+
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+
+
+
+public class ConnectionFactory {
+
+
+
+
+
+
+
+    public static final String URL = "jdbc:mysql://localhost:3306/sakila";
+
+
+
+    public static final String USER = "insertYourUsername";
+
+
+
+    public static final String PASS = "insertYourPassword";
+
+
+
+
+
+
+
+    public static Connection getConnection()
+
+
+
+    {
+
+
+
+        try {
+
+
+
+            DriverManager.registerDriver(new Driver());
+
+
+
+            return DriverManager.getConnection(URL, USER, PASS);
+
+
+
+        } catch (SQLException ex) {
+
+
+
+            throw new RuntimeException("Error connecting to the database", ex);
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
