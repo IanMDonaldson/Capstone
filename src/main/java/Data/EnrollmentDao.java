41e98578927e5e54b6abb92adfabb5e5e539e77c
dragomundo <dragomundo@oulook.com>
2020-11-15 16:48:13 -0600

    pre-merge justyn_branch

--- src/main/java/Data/EnrollmentDao.java
+++
@@ -1,9 +0,0 @@
-package Data;
-
-import java.util.List;
-
-public interface EnrollmentDao {
-   public boolean addEnrollment(Enrollment enrollment);
-    public boolean updateEnrollment(Enrollment enrollment);
-    public List<Enrollment> GetAllEnrollment();
-}
